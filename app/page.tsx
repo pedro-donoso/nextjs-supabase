81f96761db8dbab638753d4a049d0c18ee9ae964
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-14 18:27:13 -0300

    ajusto margen

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import Hero from "@/components/hero-section";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Home() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      <main className="flex-1 flex flex-col gap-4 px-4 bg-red-800 pt-4 pb-4">
+        <h2 className="font-medium text-xl mb-4 text-center text-white">
+          Título
+        </h2>
+        <div className="flex justify-center">
+          <div className="p-4 max-w-4xl w-full text-center bg-white rounded-md shadow-md">
+            <p className="text-gray-700 text-lg mb-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+              laudantium nulla quaerat sed, similique esse doloribus omnis
+              accusamus quibusdam error excepturi necessitatibus qui ea ab?
+              Velit, dolorem maiores enim, quasi, explicabo nam qui aspernatur
+              assumenda temporibus aperiam repellendus officia eius? Accusantium
+              libero saepe quod soluta voluptatem, magni iusto labore vel
+              reprehenderit id necessitatibus hic voluptates esse non laborum
+              cum, provident quae iste sunt est? Repudiandae.
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
